4a499f389f06e4de449f1084743c599ade2eb566
Kunal Dey <2025sl93xxx@wilp.bits-pilani.ac.in>
2026-04-01 13:57:07 +0530

    Add divide functionality

--- calculator.py
+++ calculator.py
@@ -1,8 +1,11 @@
-def add(a,b):
-    return a + b 
+def add(a, b):
+    return a + b
 
-def multiply(a,b):
+def multiply(a, b):
     return a * b
 
-def substract(a,b):
+def divide(a, b):
+    return a / b
+
+def subtract(a, b):
     return a - b
